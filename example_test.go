f5915d5fccc1850030a62411d5a88750dcacf2fe
Gregory Vincic <g@7de.se>
2020-07-20 08:07:17 +0200

    Add example showing default resource system

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package rs
+
+import "os"
+
+func Example_defaultResourceSystem() {
+	Root.Use(NewSystem()).Fexec(os.Stdout, "/bin/ls", "-R", "-l", "/")
+	// output:
+	// d--xrwxr-xr-x 1 1 /bin
+	// ----rwxr-xr-x 1 1 /bin/chmod
+	// ---xrwxr-xr-x 1 1 /bin/ls
+	// ----rwxr-xr-x 1 1 /bin/mkacc
+	// ----rwxr-xr-x 1 1 /bin/mkdir
+	// d---rwxr-xr-x 1 1 /etc
+	// d---rwxr-xr-x 1 1 /etc/accounts
+	// ----rw-r--r-- 1 1 /etc/accounts/anonymous.acc
+	// ----rw-r--r-- 1 1 /etc/accounts/root.acc
+	// drwxrwxrwxrwx 1 1 /tmp
+}
