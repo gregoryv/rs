19381c0b59424e5adb3df2eeec543b379c58232e
Gregory Vincic <g@7de.se>
2020-07-22 11:47:32 +0200

    Test Chown

--- chown_test.go
+++ chown_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/gregoryv/asserter"
 )
 
-func TestChown(t *testing.T) {
-	sys := NewSystem()
-	asRoot := Root.Use(sys)
-	ok, bad := asserter.NewErrors(t)
-
-	bad(asRoot.Exec("/bin/chown john /tmp"))
+func TestChown_asRoot(t *testing.T) {
+	asRoot := Root.Use(NewSystem())
+	ok, bad := asserter.NewFatalErrors(t)
 	ok(asRoot.Exec("/bin/mkacc john"))
 	ok(asRoot.Exec("/bin/chown john /tmp"))
+	bad(asRoot.Exec("/bin/chown")).Log("missing args")
+	bad(asRoot.Exec("/bin/chown john /nosuch")).Log("missing resource")
+	bad(asRoot.Exec("/bin/chown clark /tmp")).Log("account missing")
 }
