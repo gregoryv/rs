b13c15b4ecea7c2c0347a8e90d1090ade1757d4d
Gregory Vincic <g@7de.se>
2021-03-10 19:11:55 +0100

    Add Test_mem

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,7 @@ package rs
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -16,5 +17,29 @@ func Benchmark_Stat(b *testing.B) {
 	asRoot := Root.Use(NewSystem())
 	for i := 0; i < b.N; i++ {
 		asRoot.Stat("/etc/accounts.gob")
+	}
+}
+
+func Test_mem(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	asRoot := Root.Use(NewSystem())
+
+	kb := 1024
+	fileSize := 4 * kb
+	content := strings.Repeat("x", fileSize)
+	n := 4000
+	for i := 0; i < n; i++ {
+		dir := fmt.Sprintf("/tmp/dir%d", i)
+
+		asRoot.Mkdir(dir, 00755)
+		filename := fmt.Sprintf("%s/file.gob", dir)
+
+		err := asRoot.Save(filename, content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
 	}
 }
